030948149177438be309886d4f00a455264da1d4
Johan Deckmar <deckmar@gmail.com>
2015-09-08 20:10:25 +0200

    Javascript which is injected into YT page and removes all videos with a "watched" badge

---
+++ yt-remove-watched.js
@@ -0,0 +1,26 @@
+function upTo(el, tagName, andClassName) {
+  tagName = tagName.toLowerCase();
+
+  while (el && el.parentElement) {
+    el = el.parentElement;
+    if (el.tagName && el.tagName.toLowerCase() == tagName && el.className == andClassName) {
+      return el;
+    }
+  }
+
+  return null;
+}
+
+var removeWatched = function() {
+	try {
+		// All wathced-badges (note that the list "w" will shrink every time we remove an element)
+		var w = document.getElementsByClassName('watched-badge');
+		while(w.length > 0) {
+			upTo(w[0], 'li', '').remove();
+		}
+	} catch(ex) { }
+};
+
+removeWatched();
+
+setInterval(removeWatched, 500);
