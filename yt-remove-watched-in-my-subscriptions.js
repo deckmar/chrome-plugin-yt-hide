3cb37d0a0b86423f2bf8916c64098b03cda4763e
Johan Deckmar <deckmar@gmail.com>
2017-03-06 19:12:51 +0100

    Update after YT style changes

--- yt-remove-watched-in-my-subscriptions.js
+++ yt-remove-watched-in-my-subscriptions.js
@@ -14,13 +14,17 @@ function upTo(el, tagName, andClassName) {
 var removeWatched = function() {
 	try {
 		// All wathced-badges (note that the list "w" will shrink every time we remove an element)
-		var w = document.getElementsByClassName('watched-badge');
-		while(w.length > 0) {
-			upTo(w[0], 'li', 'yt-shelf-grid-item').remove();
-		}
-	} catch(ex) { }
+    do {
+      var w = document.querySelectorAll('yt-formatted-string.ytd-thumbnail-overlay-playback-status-renderer');
+      if (w.length === 0) return;
+      //console.log("Found " + w.length)
+      //console.log("Removing one")
+			var e = upTo(w[0], 'ytd-grid-video-renderer', 'style-scope ytd-grid-renderer');
+      e.remove();
+		} while(w.length > 0);
+	} catch(ex) { console.warn(ex) }
 };
 
 removeWatched();
 
-setInterval(removeWatched, 500);
+setInterval(removeWatched, 1000);
